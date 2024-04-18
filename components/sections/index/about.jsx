7d875a2e1afca46ea2dd4807a3f895546578aeae
Nuno Namora <nunonamora@proton.me>
2024-04-18 22:39:50 +0100

    update about photo and fix css

--- components/sections/index/about.jsx
+++ components/sections/index/about.jsx
@@ -34,7 +34,7 @@ export default function About() {
 				/>
 				<section className={about.content}>
 					<div className={about.image}>
-						<img src="/img/family-photo.jpg" alt="Nelson family photo"/>
+						<img src="/img/nunonamora.jpg" alt="Nuno photo"/>
 						{/* <Image src="/img/family-photo.jpg" width={600} height={800}/> */}
 					</div>
 					<div className={about.copy} >
