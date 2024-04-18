19216b1d2f88e009d1463657c9f1d9e627c0d44f
Nuno Namora <nunonamora@proton.me>
2024-04-18 15:29:16 +0100

    update files settings

--- tests/unit/sections/hero.test.jsx
+++ tests/unit/sections/hero.test.jsx
@@ -8,7 +8,7 @@ describe('Hero section parts', () => {
 		render(<Hero />)
 		
 		const heading = screen.getByRole('heading', {
-			name: /Andrew Nelson\./i,
+			name: /Nuno Namora\./i,
 		})
 		
 		expect(heading).toBeInTheDocument()
